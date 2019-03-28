7ded03fb6ae41ce40d8b1dd166f47e1c5f59c2da
illu <immu>
2019-03-28 20:08:54 +0530

    java files

---
+++ com/first.java
@@ -0,0 +1,8 @@
+package com;
+import com.pkg1.Second;
+public class First{
+           public static void main(String[] args)
+           {
+           Second.main(null);
+	   }
+} 
---
+++ com/pkg1/Second.java
@@ -0,0 +1,8 @@
+package com.pkg1;
+public class Second
+{
+public static void main(String args[])
+{
+System.out.println("HEllo World");
+}
+}
